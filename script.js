1f2a5296a845cc659c0a9cf322e53e9fb653f6d2
yogniku <156093318+yogniku@users.noreply.github.com>
2024-03-12 23:42:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width: number, height: number){
+		this.width=width: number
+	}
+	getWidth(){
+		return width: number;
+	}
+	getHeight(){
+		return height:number;
+	}
+	getArea(){
+		return ;
+	}
+}
 
-class Square extends Animal {}
+
+class Square extends Animal {
+	constructor(side: number){
+		
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
