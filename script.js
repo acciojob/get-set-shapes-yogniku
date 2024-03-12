8275ffd697f47206ad97771e65ee4b0a3297d82c
yogniku <156093318+yogniku@users.noreply.github.com>
2024-03-12 23:47:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,7 +20,16 @@ class Square extends Animal {
 		
 	}
 }
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width); // Output: 5
+console.log(rectangle.height); // Output: 10
+console.log(rectangle.getArea()); // Output: 50
 
+const square = new Square(7);
+console.log(square.width); // Output: 7
+console.log(square.height); // Output: 7
+console.log(square.getArea()); // Output: 49
+console.log(square.getPerimeter()); // Output: 28
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
